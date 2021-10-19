44fe183453d1cca56a72b7ea125b85c2c21f9c29
keshibat <kensukeshibata@gmail.com>
2021-10-19 17:16:14 +0900

    add theme

--- src/features/restaurants/screen/restaurants.screen.js
+++ src/features/restaurants/screen/restaurants.screen.js
@@ -1,17 +1,19 @@
 import React from "react";
-import styled from "styled-components/native";
 import { Searchbar } from "react-native-paper";
 import { StatusBar, SafeAreaView } from "react-native";
+import styled from "styled-components/native";
+
 import { RestaurantInfoCard } from "../componets/restaurant-info-card.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
   padding: 16px;
-  color: ${(props) => props.theme.colors.ui.error};
+  color: yellow;
+  /* color: ${(props) => props.theme.colors.ui.error}; */
 `;
 
 const RestaurantListContainer = styled.View`
