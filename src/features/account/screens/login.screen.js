8644144103f48a6e862b638d626cc127037b38dc
keshibat <kensukeshibata@gmail.com>
2021-11-30 21:54:10 +0900

    ftb

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -22,7 +22,7 @@ export const LoginScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
-      <Title>Meals To Go</Title>
+      <Title>Sober Eats</Title>
       <AccountContainer>
         <AuthInput
           label="E-mail"
