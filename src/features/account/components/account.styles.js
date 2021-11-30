35a2bed556d502c1987a9923a51c321e505b8d50
keshibat <kensukeshibata@gmail.com>
2021-11-30 16:01:08 +0900

    fix error

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -14,7 +14,7 @@ export const AccountBackground = styled.ImageBackground.attrs({
 export const AccountCover = styled.View`
   position: absolute;
   width: 100%;
-  height: 100%auto;
+  height: 100%;
   background-color: rgba(255, 255, 255, 0.3);
 `;
 
